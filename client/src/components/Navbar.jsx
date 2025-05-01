53abe5026fc3f25e6fb0a3a63158419ea4b89f9e
sumanth-dps <dondamuripavansatyasumanth@gmail.com>
2025-05-01 18:00:14 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
           onClick={() => {
             naviagte("/login");
           }}
-          className="flex items-center gap-2 border  border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
+          className="flex items-center gap-2 border  border-gray-300 rounded-full px-6 py-2 text-gray-300 hover:bg-gray-100 transition-all"
         >
           Login <img src={assets.arrow_icon}></img>
         </button>
